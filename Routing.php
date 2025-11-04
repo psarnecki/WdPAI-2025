e585fd30ee6f07e750f2557e3f44e6ca6a36296f
psarnecki <piotr.sarnecki03@gmail.com>
2025-11-04 17:09:05 +0100

    Add DashboardController and update dashboard styles and view

--- Routing.php
+++ Routing.php
@@ -1,7 +1,11 @@
 <?php
 
 require_once 'src/controllers/SecurityController.php';
+require_once 'src/controllers/DashboardController.php';
 
+// TODO musimy zapewnic ze utworzony obiekt ma tylko jedna isntancje - wzorzec singleton
+// TODO 2 /dashboard -- wszystkie dane
+// /dashboard/12343 -- dane konkretnego uzytkownika REGEX
 class Routing {
 
     public static $routes = [
@@ -12,21 +16,25 @@ class Routing {
         "register" => [
             "controller" => "SecurityController",
             "action" => "register"
+        ],
+        "dashboard" => [
+            "controller" => "DashboardController",
+            "action" => "index"
         ]
     ];
 
     public static function run($path) {
         switch($path) {
             case 'dashboard':
-                include 'public/views/dashboard.html';
-                break;
             case 'login':
             case 'register':
                 $controller = Routing::$routes[$path]['controller'];
                 $action = Routing::$routes[$path]['action'];
 
                 $controllerObj = new $controller;
-                $controllerObj->$action();
+                $id = null;
+
+                $controllerObj->$action($id);
                 break;
             default:
                 include 'public/views/404.html';
