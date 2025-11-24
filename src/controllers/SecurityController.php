d3acc4564c5fbd2b1c41163262596b77ed456d3f
psarnecki <piotr.sarnecki03@gmail.com>
2025-11-24 13:16:29 +0100

    Add registration page and update login styles/controllers

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -5,10 +5,23 @@ require_once 'AppController.php';
 class SecurityController extends AppController {
 
     public function login() {
-        return $this->render('login', ["message" => "Błędne dane logowania!"]);
+        //var_dump('GET', $_GET);
+        //var_dump('POST', $_POST);
+
+        if(!$this->isPost()) {
+            return $this->render("login");
+        }
+
+        $email = $_POST['email'] ?? '';
+        $password = $_POST['password'] ?? '';
+
+        //return $this->render('login', ["messages" => "Błędne dane logowania!"]);
+        //return $this->render('dashboard', ["cards" => []]);
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("Location: {$url}/dashboard");
     }
 
-        public function register() {
-        return $this->render('register', ["message" => "Rejestracja nie powiodła się!"]);
+    public function register() {
+        return $this->render("register");
     }
 }
